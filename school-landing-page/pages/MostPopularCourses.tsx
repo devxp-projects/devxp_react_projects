6115a413fa8c4d29b9a5b9125a4dae821b2586a4
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-03-11 15:00:34 +0100

    workspace setup

---
+++ school-landing-page/pages/MostPopularCourses.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+
+
+const MostPopularCourses = () => {
+  return (
+    <Box>
+      
+    </Box>
+  );
+};
+export default MostPopularCourses;
+    
--- school-landing-page/pages/index.tsx
+++ school-landing-page/pages/index.tsx
@@ -2,10 +2,12 @@ import Footer from "./Footer";
 import MeetOurInstructors from "./MeetOurInstructors";
 import { Box } from "@mui/material";
 import Reviews from "./Reviews";
+import MostPopularCourses from "./MostPopularCourses"
 
 export default function Index() {
   return (
     <Box sx={{ width: "80rem" }}>
+      <MostPopularCourses />
       <Reviews />
 
       <MeetOurInstructors />
