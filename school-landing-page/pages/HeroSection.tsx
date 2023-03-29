c5f6226e862439beb6479063876be377cd47c2c5
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-03-29 15:41:12 +0100

    Added template for Herosection component

---
+++ school-landing-page/pages/HeroSection.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+
+
+
+const HeroSection = () => {
+  return (
+
+    <Container>
+        <Grid>
+            
+        </Grid>
+    </Container>
+    
+      
+  );
+};
+export default HeroSection;
+    
