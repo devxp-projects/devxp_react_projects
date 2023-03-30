160d89a840e2a69be55de81a868169f8e2440655
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-03-30 18:50:01 +0100

    heroSection component added

--- school-landing-page/pages/HeroSection.tsx
+++ school-landing-page/pages/HeroSection.tsx
@@ -11,29 +11,49 @@ import Grid from '@mui/material/Grid';
 const HeroSection = () => {
   return (
 
-    <Container sx={{overflowX: "hidden", marginBottom: "2rem", marginTop: "4rem"}}>
-        <Grid container justifyContent="space-between" sx={{width: "100%", marginBottom: "10px"}}>
-        <Grid item>
+    <Container sx={{overflowX: "hidden", marginBottom: "2rem", marginTop: "4rem", marginLeft: "auto"}}>
+        <Grid container justifyContent="center" alignItems="center" sx={{width: "100%", marginBottom: "10px"}}>
+        <Grid item xs={12} md={6}>
           <Box>
-            <Typography variant="h2" display="inline" sx={{marginRight: "2px", fontWeight: 600}}>Quality</Typography>
-            <Typography variant="h2" display="inline" sx={{color: "#09B451", fontweight: 600}}>Education</Typography>
-            <Typography variant="h2" sx={{fontWeight: 600}}>By Any Means Necessary.</Typography>
-            <Button variant="contained" sx={{marginTop: "5px", borderRadius: "50px", fontSize: "12px", padding: "8px 16px 8px 16px", marginRight: "20px",
+            <Typography variant="h3" display="inline" sx={{marginRight: "4px", fontWeight: 600}}>Quality</Typography>
+            <Typography variant="h3" display="inline" sx={{color: "#09B451", fontWeight: 600}}>Education</Typography>
+            <Typography variant="h3" sx={{fontWeight: 600}}>By Any Means</Typography>
+            <Typography variant="h3" sx={{fontWeight: 600}}>Necessary.</Typography>
+            <Button variant="contained" sx={{marginTop: "10px", borderRadius: "5px", fontSize: "12px", padding: "20px 42px 20px 42px", marginRight: "20px",
             backgroundColor: "#09B451", color: "#FFFFFF"}}>Get Started</Button>
           </Box>
           
         </Grid>
-        <Grid item>
-          <Box>
-            <img src="../src/assets/images/hero_img.PNG"></img>
+        <Grid item xs={12} md={6}>
+          <Box sx={{width: "100%"}}>
+            <img src="../src/assets/images/hero_img.PNG" width= "100%"></img>
           </Box>
         
         </Grid>
       </Grid>
 
-      <Box sx={{display: "flex", justifyContent: "space-between"}}>
-        
-      </Box>
+      
+
+      
+        <Grid container justifyContent="space-between">
+          <Grid item>
+            <img src="../src/assets/images/one.png"></img>
+          </Grid>
+          <Grid item>
+            <img src="../src/assets/images/two.png"></img>
+          </Grid>
+          <Grid item>
+            <img src="../src/assets/images/three.png"></img>
+          </Grid>
+          <Grid item>
+            <img src="../src/assets/images/four.png"></img>
+          </Grid>
+          <Grid item>
+            <img src="../src/assets/images/five.png"></img>
+          </Grid>
+        </Grid>
+      
+    
     </Container>
     
       
