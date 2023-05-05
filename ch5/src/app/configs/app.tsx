d1f3588dfbe085b2c98499356de3f6b85e7ba283
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-05-05 17:39:57 +0100

    added app.tsx here to hold app info

---
+++ ch5/src/app/configs/app.tsx
@@ -0,0 +1,53 @@
+import AnnouncementIcon from '../assets/icons/messagebox.svg';
+
+export const Dummy = [
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+    {
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization.", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+    },
+];
+
+export const Icons = {
+    unread: AnnouncementIcon
+}
+
+export const Fonts = {
+    size: {
+        base: 0.8125,
+        small: 0.75,
+        large: 1.25,
+    },
+
+    weight: {
+        thin: 100,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        bold: 700,
+        black: 900
+    }
+}
+
+export const Colors = {
+    base: '#000000'
+}
