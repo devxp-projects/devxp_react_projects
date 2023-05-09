7b29b5e04fdec23b9fc2f54a81005d71d68c9fe8
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-05-09 22:26:30 +0100

    added id key to announcements, didn't use it tho

--- ch5/src/app/configs/app.tsx
+++ ch5/src/app/configs/app.tsx
@@ -1,66 +1,69 @@
 import AnnouncementIcon from '../assets/icons/messagebox.svg';
 import nextIcon from '../assets/icons/nextIcon.svg';
 import previousIcon from '../assets/icons/previousIcon.svg';
+import hamburger from '../assets/icons/hamburger.svg'
+import { DummyDataProps } from '../types/components.types';
 
 
-export const Dummy = [
+export const announcements: DummyDataProps = [
     {
-        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization. ", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or"
+        date: 'Feb 11', title: "Zalora- Seller Communication Prioritization. ", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or", id: 1
     },
     {
-        date: 'Feb 11', title: "[IMPORTANT] Update to API endponit limit.", description: "Dear Seller, Follewing up from our previous announcement, please note a slight change"
+        date: 'Feb 11', title: "[IMPORTANT] Update to API endponit limit.", description: "Dear Seller, Follewing up from our previous announcement, please note a slight change", id: 2
     },
     {
-        date: 'Feb 11', title: "Covid-19 Impact on Your Operations.", description: "Dear Seller, please immediately inform your Account. Manage or SHD if your operations are impacted by"
+        date: 'Feb 11', title: "Covid-19 Impact on Your Operations.", description: "Dear Seller, please immediately inform your Account. Manage or SHD if your operations are impacted by", id: 3
     },
     {
-        date: 'Feb 11', title: "[IMPORTANT] API endponit limit. please forward.", description: "Dear Seller, As part of our continuous efforts to improve system response time and reliability"
+        date: 'Feb 11', title: "[IMPORTANT] API endponit limit. please forward.", description: "Dear Seller, As part of our continuous efforts to improve system response time and reliability", id: 4
     },
     {
-        date: 'Feb 11', title: "Number Five", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Five", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 5
     },
     {
-        date: 'Feb 11', title: "Number Six", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Six", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 6
     },
     {
-        date: 'Feb 11', title: "Number Seven", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Seven", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 7
     },
     {
-        date: 'Feb 11', title: "Number Eight", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Eight", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 8
     },
     {
-        date: 'Feb 11', title: "Number Nine", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Nine", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 9
     },
     {
-        date: 'Feb 11', title: "Number Ten", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Ten", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 10
     },
     {
-        date: 'Feb 11', title: "Number Eleven", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Eleven", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 11
     },
     {
-        date: 'Feb 11', title: "Number Twelve", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Twelve", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 12
     },
     {
-        date: 'Feb 11', title: "Number Thirteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Thirteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 13
     },
     {
-        date: 'Feb 11', title: "Number Fourteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Fourteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 14
     },
     {
-        date: 'Feb 11', title: "Number Fifteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Fifteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id:15 
     },
     {
-        date: 'Feb 11', title: "Number Sixteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Sixteen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 16
     },
     {
-        date: 'Feb 11', title: "Number Seventeen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ..."
+        date: 'Feb 11', title: "Number Seventeen", description: "Hi, you’ve likely seen email from Zalora Seller Helpdesk to you with the Phrase [HIGH] or ...", id: 17
     }
 ];
 
 export const Icons = {
     unread: AnnouncementIcon,
     previous: previousIcon,
-    next: nextIcon
+    next: nextIcon,
+    hamburger: hamburger
 }
 
 export const Fonts = {
