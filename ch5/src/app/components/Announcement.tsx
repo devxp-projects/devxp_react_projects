bc78fa0eb881b0852df72ae9eef1d3086b3100a2
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-05-09 22:25:45 +0100

    Update: Implemented search functionality

--- ch5/src/app/components/Announcement.tsx
+++ ch5/src/app/components/Announcement.tsx
@@ -1,37 +1,60 @@
-import { useState } from "react"
-import { Dummy, Icons } from "../configs/app"
-import { Card } from "./Card"
-import { Column, Row } from "./Flex"
-import { H1, H4, Text } from "./Typography"
+import React, { useState } from "react";
+import { Icons, announcements } from "../configs/app";
+import { Card } from "./Card";
+import { Column, Row } from "./Flex";
+import { H1, H4, Text } from "./Typography";
+import { Search } from "./Search";
 
-export const Announcement = () => {
+const Announcements = () => {
     const tabSize = 4;
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(tabSize);
-    const data = Dummy.slice(start, end);
+    const [showingAnnouncements, setShowingAnnouncements] = useState(
+        announcements.slice(start, end)
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [searchValue, setSearchValue] = useState("");
 
     const handleNext = () => {
-        if (end >= Dummy.length) {
+        if (end >= announcements.length) {
             return;
         }
-        setStart(start + data.length);
-        setEnd(end + data.length);
+        setStart(start + tabSize);
+        setEnd(end + tabSize);
+        setShowingAnnouncements(
+            announcements.slice(start + tabSize, end + tabSize)
+        );
     };
 
-    const handlePrevious = () => {
+    const handlePrev = () => {
         if (start === 0) {
             return;
         }
         setStart(start - tabSize);
-        setEnd(end - tabSize);
+        setEnd(end - tabSize)
+        setShowingAnnouncements(
+            announcements.slice(start - tabSize, end - tabSize)
+        );
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+        const filteredAnnouncements = announcements.filter((announcement) =>
+            announcement.title.toLowerCase().includes(e.target.value.toLowerCase())
+        );
+        setShowingAnnouncements(filteredAnnouncements.slice(0, tabSize));
     };
 
     return (
         <Card borderRadius={0.5}>
-            <Row padding={2} borderBottom="1px solid #DBDADE;">
+            <Row padding={2} borderBottom="1px solid #DBDADE;" alignItems="center" justifyContent="space-between" >
                 <H1>Announcements</H1>
+                <Row>
+                    <Search placeholder="Search" onChange={handleInputChange} />
+                    <img src={Icons.hamburger} alt="hamburger-menu"></img>
+                </Row>
             </Row>
-            {data.map((update, i) => {
+            {showingAnnouncements.map((update, i) => {
                 return (
                     <Row key={i} justifyContent="space-between" alignItems="center" gap={2} padding={2} borderBottom="1px solid #DBDADE">
                         <Text color={"#DBDADE"}>{update.date}</Text>
@@ -44,12 +67,14 @@ export const Announcement = () => {
                 )
             })}
             <Row alignItems="center" justifyContent="space-between" padding={2} borderBottom="1px solid #DBDADE" >
-                <Text fontWeight={400} color="#979797" >Showing {start + 1} to {(data.length < tabSize) ? start + data.length : end} of {Dummy.length} </Text>
+                <Text fontWeight={400} color="#979797" >Showing {start + 1} to {(showingAnnouncements.length < tabSize) ? start + showingAnnouncements.length : end} of {announcements.length} </Text>
                 <Row alignItems="center" gap={0.75} >
-                    <img src={Icons.previous} alt="previous icon" onClick={handlePrevious} ></img>
+                    <img src={Icons.previous} alt="previous icon" onClick={handlePrev} ></img>
                     <img src={Icons.next} alt="next icon" onClick={handleNext}></img>
                 </Row>
             </Row>
-        </Card>
-    )
-}
+        </Card >
+    );
+};
+
+export default Announcements;
