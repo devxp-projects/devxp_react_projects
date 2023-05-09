29b1791d631fa8d1539e2ba38d419c9cfdbad0b9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-09 10:31:38 +0100

    feat: Create Sidebar and add logo

---
+++ ch5/src/app/components/Sidebar.tsx
@@ -0,0 +1,40 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import Logo from '../assets/logo.png';
+const Sidebar = () => {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        width: 255,
+        backgroundColor: '#363740',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '12px 0px 43px',
+        alignItems: 'flex-end',
+      }}
+    >
+      {/* Logo */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '0px 20px',
+        }}
+      >
+        <Box
+          component="img"
+          sx={{
+            width: '100%',
+          }}
+          alt="Logo"
+          src={Logo}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
