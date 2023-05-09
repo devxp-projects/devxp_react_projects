eeb9888f052ece3e84aed152c32cc91143ed674a
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-05-09 15:32:33 +0100

    update: added salesSummaryRow

--- ch5/src/app/app.tsx
+++ ch5/src/app/app.tsx
@@ -1,19 +1,13 @@
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from "./nx-welcome";
-
+import SalesSummaryRow from "./SalesSummaryRow/SalesSummaryRow";
+import "./app.module.scss";
 
 export function App() {
-
   return (
     <>
-      <NxWelcome title="ch5"/>
-      <div />
-    </>);
-
+      <SalesSummaryRow />
+    </>
+  );
 }
 
-
 export default App;
-
