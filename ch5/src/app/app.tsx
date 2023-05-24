9b32028fc2a05946ba2328d1180395eb4f35d705
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-05-24 21:42:31 +0100

    feat: add First row cards

--- ch5/src/app/app.tsx
+++ ch5/src/app/app.tsx
@@ -1,15 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
+import FirstRowCards from "./FirstRowCards/FirstRowCards";
 import SalesSummaryRow from "./SalesSummaryRow/SalesSummaryRow";
 import "./app.module.scss";
 // import styles from './app.module.scss';
 // import NxWelcome from "./nx-welcome";
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
 export function App() {
-
   return (
     <>
+      <FirstRowCards />
       <SalesSummaryRow />
       {/* <NxWelcome title="ch5" /> */}
       {/* <div /> */}
