df70a3ad1deffd44a681de9a0312ac10319fc7eb
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-05-29 12:45:33 +0100

    fix: duplicate bugs

--- ch5/src/app/app.tsx
+++ ch5/src/app/app.tsx
@@ -1,25 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-import FirstRowCards from "./FirstRowCards/FirstRowCards";
-import SalesSummaryRow from "./SalesSummaryRow/SalesSummaryRow";
 import "./app.module.scss";
-// import styles from './app.module.scss';
-// import NxWelcome from "./nx-welcome";
+import "./app.module.scss";
 import Layout from "./components/Layout";
-import "./app.module.scss";
-import Layout from './components/Layout';
 import { ContextProvider } from "./context/Context";
+// import NxWelcome from "./nx-welcome";
 
 export function App() {
   return (
     <>
-      <FirstRowCards />
-      <SalesSummaryRow />
       {/* <NxWelcome title="ch5" /> */}
       {/* <div /> */}
-    <ContextProvider>
-      <Layout />
-    </ContextProvider>
+      <ContextProvider>
+        <Layout />
+      </ContextProvider>
+    </>
   );
 }
 
--- ch5/src/app/components/Layout.tsx
+++ ch5/src/app/components/Layout.tsx
@@ -1,30 +1,31 @@
-import { useContext, useState } from 'react';
-import { Box, ThemeProvider } from '@mui/material';
-import Sidebar from './Sidebar';
-import Announcement from './Announcement';
-import { theme } from '../themes';
-import SalesSummaryRow from '../SalesSummaryRow/SalesSummaryRow';
-import { TopNav } from './TopNav';
-import { Context } from '../context/Context';
-import { SearchResults } from './SearchResults';
-import { PromotionalCards } from './PromotionalCards';
+import { useContext, useState } from "react";
+import { Box, ThemeProvider } from "@mui/material";
+import Sidebar from "./Sidebar";
+import Announcement from "./Announcement";
+import { theme } from "../themes";
+import SalesSummaryRow from "../SalesSummaryRow/SalesSummaryRow";
+import { TopNav } from "./TopNav";
+import { Context } from "../context/Context";
+import { SearchResults } from "./SearchResults";
+import { PromotionalCards } from "./PromotionalCards";
+import FirstRowCards from "../FirstRowCards/FirstRowCards";
 
 export const baseStyle = {
   display: "flex",
   flexDirection: "column",
   gap: "2rem",
-  background: '#DBDADE',
+  background: "#DBDADE",
   flexGrow: 1,
-  padding: '2rem',
-}
+  padding: "2rem",
+};
 
 const Layout = () => {
   const [openSidebar, setOpenSidebar] = useState(true);
   const { showSearchResults } = useContext(Context);
 
   return (
-    <ThemeProvider theme={theme} >
-      <Box sx={{ display: 'flex', backgroundColor: '#DBDADE', }}>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex", backgroundColor: "#DBDADE" }}>
         <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
         <Box
           sx={{
@@ -36,17 +37,24 @@ const Layout = () => {
         >
           {showSearchResults && <SearchResults />}
           {!showSearchResults && <TopNav />}
-          <Box sx={{ ...baseStyle, padding: "0", flexDirection: "row", "@media (max-width: 850px)": {flexDirection: "column-reverse"} }}>
+          <Box
+            sx={{
+              ...baseStyle,
+              padding: "0",
+              flexDirection: "row",
+              "@media (max-width: 850px)": { flexDirection: "column-reverse" },
+            }}
+          >
             <Box sx={{ ...baseStyle, padding: "0" }}>
+              {!showSearchResults && <FirstRowCards />}
               {!showSearchResults && <SalesSummaryRow />}
               {!showSearchResults && <Announcement />}
             </Box>
             {!showSearchResults && <PromotionalCards />}
           </Box>
-
         </Box>
       </Box>
-    </ThemeProvider >
+    </ThemeProvider>
   );
 };
 
