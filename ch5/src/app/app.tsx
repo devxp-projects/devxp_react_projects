2a62a371b39bb27a6f256216ff8aa4453fb8e073
Seth Samuel <sethsamuel@gmail.com>
2023-05-29 10:55:18 +0100

    Final layout and correction of project

--- ch5/src/app/app.tsx
+++ ch5/src/app/app.tsx
@@ -1,26 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-import FirstRowCards from "./FirstRowCards/FirstRowCards";
-import SalesSummaryRow from "./SalesSummaryRow/SalesSummaryRow";
 import "./app.module.scss";
-// import styles from './app.module.scss';
-// import NxWelcome from "./nx-welcome";
+import "./app.module.scss";
 import Layout from "./components/Layout";
-import "./app.module.scss";
-import Layout from './components/Layout';
 import { ContextProvider } from "./context/Context";
 
 export function App() {
-  return (
-    <>
-      <FirstRowCards />
-      <SalesSummaryRow />
-      {/* <NxWelcome title="ch5" /> */}
-      {/* <div /> */}
-    <ContextProvider>
-      <Layout />
-    </ContextProvider>
-  );
+    return (
+        <>
+            {/* <NxWelcome title="ch5" /> */}
+            {/* <div /> */}
+            <ContextProvider>
+                <Layout />
+            </ContextProvider>
+        </>
+    );
 }
 
 export default App;
